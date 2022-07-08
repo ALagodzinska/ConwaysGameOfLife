d37d5f8af2c1dde0f95ddb00af1b5ba7ce154e4b
Anastasija Lagodzinska <a.lagodzinska@accenture.com>
2022-07-08 11:30:16 +0300

    Added method that allows player to set game field

--- ConwaysGameOfLife/GameLogic.cs
+++ ConwaysGameOfLife/GameLogic.cs
@@ -3,7 +3,7 @@
     public class GameLogic
     {
         public List<Cell> cells = new List<Cell>();
-        public void CreateGrid(int height, int width)
+        public void CreateRandomGrid(int height, int width)
         {
             Thread.Sleep(1000);
             Console.Clear();
@@ -22,13 +22,104 @@
                     else
                     {
                         cells.Add(cell);
-                        Console.Write(" ");
+                        Console.Write("O");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
             Console.WriteLine("Count of iteration: 0");
             Console.WriteLine($"Count of live cells: {CountOfLiveCells()}");
+        }
+
+        public void CreateYourOwnGrid(int maxHeight, int maxWidth)
+        {
+            CreateEmptyGrid(maxHeight, maxWidth);
+            bool setField = true;
+            Console.SetCursorPosition(0, 0);
+
+            while (setField)
+            {
+                var startPosition = Console.GetCursorPosition();
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.Spacebar:
+                        Console.SetCursorPosition(startPosition.Left, startPosition.Top);
+                        var cell = FindCellByCoordinates(startPosition.Top, startPosition.Left);
+                        Console.Write("X");
+                        cell.IsLive = true;
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        if (startPosition.Top - 1 < 0)
+                        {
+                            Console.SetCursorPosition(startPosition.Left, startPosition.Top = maxHeight - 1);
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(startPosition.Left, startPosition.Top - 1);
+                        }
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if (startPosition.Top + 1 >= maxHeight)
+                        {
+                            Console.SetCursorPosition(startPosition.Left, startPosition.Top = 0);
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(startPosition.Left, startPosition.Top + 1);
+                        }
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        if (startPosition.Left - 1 < 0)
+                        {
+                            Console.SetCursorPosition(startPosition.Left = maxWidth - 1, startPosition.Top);
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(startPosition.Left - 1, startPosition.Top);
+                        }
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        if (startPosition.Left + 1 >= maxWidth)
+                        {
+                            Console.SetCursorPosition(startPosition.Left = 0, startPosition.Top);
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(startPosition.Left + 1, startPosition.Top);
+                        }
+                        break;
+
+                    case ConsoleKey.Enter:
+                        setField = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public void CreateEmptyGrid(int height, int width)
+        {
+            Thread.Sleep(1000);
+            Console.Clear();
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    Cell cell = new Cell(h, w, false);
+                    cells.Add(cell);
+                    Console.Write("O");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("To move use ARROWS. To make cell live use SPACE. To stop setting field use ENTER.");
         }
 
         //at the moment + applying rules for 2nd generation
@@ -103,11 +194,12 @@
                     }
                     else
                     {
-                        Console.Write(" ");
+                        Console.Write("O");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
             Console.WriteLine($"Count of iteration: {count}");
             Console.WriteLine($"Count of live cells: {CountOfLiveCells()}");            
         }
--- ConwaysGameOfLife/Program.cs
+++ ConwaysGameOfLife/Program.cs
@@ -10,7 +10,7 @@ class Program
         int iterationCount;
         //bool userRequestedStop = false;
 
-        Console.WriteLine("Hello, Welcom to the 'Game Of Life'!");
+        Console.WriteLine("Hello, Welcome to the 'Game Of Life'!");
         Console.WriteLine("Please input height of field?");
         var heightInput = Console.ReadLine();
         if (!int.TryParse(heightInput, out height))
@@ -38,7 +38,8 @@ class Program
         }
 
         Console.WriteLine("Let the game begin...");
-        game.CreateGrid(height, width);
+        //game.CreateRandomGrid(height, width);
+        game.CreateYourOwnGrid(height, width);
 
         int count = 1;
         while (count <= iterationCount)
